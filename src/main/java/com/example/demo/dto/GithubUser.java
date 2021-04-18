6c303e3b3fe256d4219eb7b6c3de0e886082ac54
Close-Qun <343335439@qq.com>
2021-04-18 16:54:44 +0800

    第一部分操作

---
+++ src/main/java/com/example/demo/dto/GithubUser.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+/**
+ * @author 陈亦铖
+ */
+@Data
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String bio;
+
+}
