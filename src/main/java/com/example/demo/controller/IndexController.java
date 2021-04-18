d392e1b1c1c92d7cf8d87da884e3dd3b854eefd3
Close-Qun <343335439@qq.com>
2021-04-18 14:20:58 +0800

    add index and bootstrap

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author 陈亦铖
  */
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
+public class IndexController {
+    @GetMapping("/")
     public String hello(@RequestParam(name="name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+
+        return "index";
     }
 }
